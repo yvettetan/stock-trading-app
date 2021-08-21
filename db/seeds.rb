a9f4ef36a46c5f65ca5965818b78f830a42fb8dc
lea927 <lxthym@icloud.com>
2021-08-21 23:04:43 +0800

    Updates seed data to create role directly from user create command

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-['Admin', 'Broker', 'Buyer'].each do |role|
-    Role.find_or_create_by({name: role})
-end
+admin = Role.create!(name: 'Admin')
+broker = Role.create!(name: 'Broker')
+buyer = Role.create!(name: 'Buyer')
+
 User.create!([
-            { first_name: 'Tom', last_name: 'Reid', email: 'tom_admin@email.com', password: '123qwe' },
-            { first_name: 'Jane', last_name: 'Doe', email: 'janedoe_broker@email.com', password: '123qwe' },
-            { first_name: 'John', last_name: 'Doe', email: 'johndoe_buyer@email.com', password: '123qwe' }
+                { first_name: 'Tom', last_name: 'Reid', email: 'tom_admin@email.com', password: '123qwe', roles: [ admin ] },
+                { first_name: 'Jane', last_name: 'Doe', email: 'janedoe@email.com', password: '123qwe', roles: [ broker, buyer ] },
+                { first_name: 'John', last_name: 'Doe', email: 'johndoe@email.com', password: '123qwe', roles: [ broker, buyer ] }
             ])
-User.first.roles << Role.find_by_name(:Admin)
-User.second.roles << Role.find_by_name(:Broker)
-User.third.roles << Role.find_by_name(:Buyer)
